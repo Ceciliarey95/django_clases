7d158b155a2fea8b1e9973af3a5d7d1da2b9d856
Ceci <ceciliarey95.cr@gmail.com>
2024-10-08 22:46:58 -0300

    clase_8-10

--- prueba1/apps/libros/urls.py
+++ prueba1/apps/libros/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from .views import CrearCategoria, ActualizarCategoria, EliminarCategoria, CrearLibro, ActualizarLibro, ListarLibros, EliminarLibro
+from .views import CrearCategoria, ActualizarCategoria, EliminarCategoria, CrearLibro, ActualizarLibro, ListarLibros, EliminarLibro, detalle_libro, listar_libro_por_categoria
 
 app_name = 'apps.libros'
 
@@ -14,6 +14,8 @@ urlpatterns = [
     path("agregar_libro/", CrearLibro.as_view(), name= 'agregar_libro'),
     path("actualizar_libro/<int:pk>", ActualizarLibro.as_view(), name = 'actualizar_libro'),
     path("eliminar_libro/<int:pk>", EliminarLibro.as_view(), name= 'eliminar_libro'),
-    path("listar_libros/", ListarLibros.as_view(), name='listar_libros')
+    path("listar_libros/", ListarLibros.as_view(), name='listar_libros'),
+    path("libro/<int:id>", detalle_libro, name = 'libro'),
+    path("listar_por_categoria/<str:categoria>", listar_libro_por_categoria, name='listar_por_categoria' )
 
 ]
--- prueba1/apps/libros/views.py
+++ prueba1/apps/libros/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from mixins.custom_test_mixin import CustomTestMixin
 
 from .models import Categoria, Libro
 
@@ -10,38 +13,39 @@ from .models import Categoria, Libro
 # Create your views here.
 
 # ------- Categorias -----------------
-class CrearCategoria(LoginRequiredMixin, CreateView):
+
+class CrearCategoria(LoginRequiredMixin,CustomTestMixin, CreateView):
     model = Categoria
     fields = ['nombre']
     template_name = 'libros/agregar_categoria.html'
     success_url = reverse_lazy('index')
 
-class ActualizarCategoria(UpdateView):
+class ActualizarCategoria(UpdateView, CustomTestMixin):
     model = Categoria
     fields = ['nombre']
     template_name = 'libros/agregar_categoria.html'
     success_url = reverse_lazy('index')
 
-class EliminarCategoria(DeleteView):
+class EliminarCategoria(DeleteView, CustomTestMixin):
     model = Categoria
     template_name = 'genericos/confirma_eliminar.html'
     success_url = reverse_lazy('index')
 
 # ------- Libros -----------------
 
-class CrearLibro(CreateView):
+class CrearLibro(CreateView , CustomTestMixin):
     model = Libro
     fields = ['titulo','autor','descripcion','categoria','imagen']
     template_name = 'libros/agregar_libro.html'
     success_url = reverse_lazy('index')
 
-class ActualizarLibro(UpdateView):
+class ActualizarLibro(UpdateView , CustomTestMixin):
     model = Libro
     fields = ['titulo','autor','descripcion','categoria','imagen']
     template_name = 'libros/agregar_libro.html'
     success_url = reverse_lazy('index')
 
-class EliminarLibro(DeleteView):
+class EliminarLibro(DeleteView, CustomTestMixin):
     model = Libro
     template_name = 'genericos/confirma_eliminar.html'
     success_url = reverse_lazy('index')
@@ -50,4 +54,31 @@ class ListarLibros(ListView):
     model = Libro
     template_name = 'libros/listar_libros.html'
     context_object_name = 'libros'
+
+    def get_context_data(self) :
+        context=super().get_context_data()
+        categorias = Categoria.objects.all()
+        context["categorias"] = categorias
+        return context
+
+
+
+def listar_libro_por_categoria(request, categoria):
+    categoria_db = Categoria.objects.filter(nombre = categoria)
+    libros = Libro.objects.filter(categoria = categoria_db[0].id)
+    template_name = 'libros/listar_libros.html'
+    context = {
+        'libros' : libros
+    }
+    return render(request, template_name=template_name, context=context)
+
+def detalle_libro(request,id):
+    libro = Libro.objects.get(id = id)
+
+    context= {
+        "libro": libro
+    }
+    template_name = "libros/libro_detalle.html"
+
+    return render(request, template_name=template_name,context=context)
     
